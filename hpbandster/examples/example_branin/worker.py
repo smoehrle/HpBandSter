49b7746e7eff352e8cb621725dc86cbb0f5d02c2
David-Elias Kuenstle <kuenstld@cs.uni-freiburg.de>
2018-05-09 20:07:36 +0200

    Use fidelity according to budget for branin example

--- hpbandster/examples/example_branin/worker.py
+++ hpbandster/examples/example_branin/worker.py
@@ -25,18 +25,25 @@ class MyWorker(Worker):
             For dramatization, the function sleeps for one second, which emphasizes
             the speed ups achievable with parallel workers.
         """
-        time.sleep(budget/100)
+        z1, z2, z3 = 1, budget / 100, 1
+        cost = self.cost(z1, z2, z3)
+        time.sleep(cost)
 
         x1, x2 = config['x1'], config['x2']
-        y = self.calc_noisy_branin(x1, x2)
+        y = self.calc_noisy_branin(x1, x2, z1, z2, z3)
 
         return({
             'loss': np.abs(self.true_y - y),   # this is the a mandatory field to run hyperband
             'info': 'x1: {}, x2: {}, y: {}, y_t: {}'.format(x1, x2, y, self.true_y)     # can be used for any user-defined information - also mandatory
         })
 
-    def calc_noisy_branin(self, x1, x2, noise_variance=0.05):
-        return self.calc_branin(x1, x2) + np.random.normal(0, noise_variance)
+    @staticmethod
+    def calc_noisy_branin(self, x1, x2, z1, z2, z3, noise_variance=0.05):
+        return self.calc_branin(x1, x2, z1, z2, z3) + np.random.normal(0, noise_variance)
+
+    @staticmethod
+    def cost(self, z1: float, z2: float, z3: float) -> float:
+        return 0.05 + (z1**3 * z2**2 * z3**1.5)
 
     @staticmethod
     def calc_branin(x1, x2, z1=1, z2=1, z3=1):
@@ -47,4 +54,4 @@ class MyWorker(Worker):
         s = 10
         t = 1 / (8 * np.pi) + 0.05 * (1 - z3)
 
-        return a * (x2 - b * x1**2 + c * x1 - r)**2 + s * (1-t) * np.cos(x1) + s
+        return a * (x2 - b * x1**2 + c * x1 - r)**2 + s * (1 - t) * np.cos(x1) + s
