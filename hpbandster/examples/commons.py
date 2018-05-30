7c6431a105316911df9b3642fa0ed39168a9c749
smoehrle <26185187+smoehrle@users.noreply.github.com>
2018-05-30 11:22:22 +0200

    Pep8 example_2

--- hpbandster/examples/commons.py
+++ hpbandster/examples/commons.py
@@ -10,32 +10,26 @@ logging.basicConfig(level=logging.DEBUG)
 
 class MyWorker(Worker):
 
+    def compute(self, config, budget, *args, **kwargs):
+        """
+            Simple example for a compute function
 
-	def compute(self, config, budget, *args, **kwargs):
-		"""
-			Simple example for a compute function
-			
-			The loss is just a the config + some noise (that decreases with the budget)
-			There is a 10 percent failure probability for any run, just to demonstrate
-			the robustness of Hyperband agains these kinds of failures.
-			
-			For dramatization, the function sleeps for one second, which emphasizes
-			the speed ups achievable with parallel workers.
-		"""
+            The loss is just the config + some noise (that decreases with the budget)
+            There is a 10 percent failure probability for any run, just to demonstrate
+            the robustness of Hyperband agains these kinds of failures.
 
-		#time.sleep(1)
+            For dramatization, the function sleeps for one second, which emphasizes
+            the speed ups achievable with parallel workers.
+        """
 
-		# simulate some random failure
-		if random.random() < 0.:
-			raise RuntimeError("Random runtime error!")
-		
-		
-		res = []
-		for i in range(int(budget)):
-			tmp = np.clip(config['x'] + np.random.randn()/budget, config['x']/2, 1.5*config['x'])
-			res.append(tmp)
-		
-		return({
-					'loss': np.abs(np.mean(res)),   # this is the a mandatory field to run hyperband
-					'info': res             # can be used for any user-defined information - also mandatory
-				})
+        # time.sleep(1)
+
+        res = []
+        for _ in range(int(budget)):
+            tmp = np.clip(config['x'] + np.random.randn()/budget, config['x']/2, 1.5*config['x'])
+            res.append(tmp)
+
+        return {
+            'loss': np.abs(np.mean(res)),   # this is the a mandatory field to run hyperband
+            'info': res             # can be used for any user-defined information - also mandatory
+        }
