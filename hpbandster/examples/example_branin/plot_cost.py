87fff5f04d4e305692f92ed03f56268cc9670b1c
David-Elias Kuenstle <kuenstld@cs.uni-freiburg.de>
2018-06-14 19:20:04 +0200

    Plot nice cost trajectories using another solver and objective

--- hpbandster/examples/example_branin/plot_cost.py
+++ hpbandster/examples/example_branin/plot_cost.py
@@ -7,13 +7,14 @@ import branin
 
 
 resolution = 50
-hb_steps = 10
-margin = 0.01
-cost_pow = dict(pow_z1=1, pow_z2=1, pow_z3=1.5)
+hb_steps = 5
+alpha = 1
+cost_pow = dict(pow_z1=3, pow_z2=2, pow_z3=1.5)
 min_cost = branin.cost(0, 0, 0, **cost_pow)
 max_cost = branin.cost(1, 1, 1, **cost_pow)
+extent = (0, 1, 0, 1)
 
-z = np.linspace(0, 1 + margin, resolution, endpoint=True)
+z = np.linspace(0, 1, resolution, endpoint=True)
 Z1, Z2, Z3 = np.meshgrid(z, z, z, sparse=True)
 C = branin.cost(Z1, Z2, Z3, **cost_pow)
 budgets = min_cost + (max_cost - min_cost) * 3.**np.arange(1 - hb_steps, 1)
@@ -33,29 +34,35 @@ def calc_trajectory(xi, yi):
         zz = np.ones(3)
         zz[xi] = z[0]
         zz[yi] = z[1]
-        return np.abs(branin.cost(*zz, **cost_pow) - b) - np.linalg.norm(zz, ord=2)
+        return 3**np.abs(log3(branin.cost(*zz, **cost_pow)) - log3(b)) - alpha * np.linalg.norm(zz, ord=2)
     trajectory = np.empty((len(budgets), 2))
     x0 = np.ones(2)
     for i, b in enumerate(budgets[::-1]):
-        z = minimize(traj_func, x0, b, bounds=[(0, 1 + margin), (0, 1 + margin)])['x']
+        # Linesearch fails too often for default solver L-BFGS
+        # Investigate why linesearch fails - they are also an issue with TNC
+        options = dict(maxiter=1000)
+        result = minimize(traj_func, x0, b, method='TNC', bounds=[(0, 1), (0, 1)], options=options)
+        z = result['x']
+        print(result)
         trajectory[i] = z
         x0 = z
     return trajectory
 
 
-extent = (0, 1 + margin, 0, 1 + margin)
-for i, (xl, yl, CC) in enumerate(plot_data):
-    plt.title('$cost \\propto z_{0}^{{{1}}} \\cdot z_{2}^{{{3}}}$'
-              .format(xl, cost_pow['pow_z' + str(xl)],
-                      yl, cost_pow['pow_z' + str(yl)]))
-    plt.xlabel('$z_{}$'.format(xl))
-    plt.ylabel('$z_{}$'.format(yl))
-    im = plt.imshow(CC, interpolation='bilinear', origin='lower', extent=extent)
-    co = plt.contour(CC, levels=budgets, colors='k', origin='lower', extent=extent)
-    plt.clabel(co, budgets[0::2],  # label every second level
+f, axis = plt.subplots(1, 3, sharey=True, figsize=(15, 5))
+
+for ax, (xl, yl, CC) in zip(axis, plot_data):
+    ax.set_title('$cost \\propto z_{0}^{{{1}}} \\cdot z_{2}^{{{3}}}$'
+                 .format(xl, cost_pow['pow_z' + str(xl)],
+                         yl, cost_pow['pow_z' + str(yl)]))
+    ax.set_xlabel('$z_{}$'.format(xl))
+    ax.set_ylabel('$z_{}$'.format(yl))
+    im = ax.imshow(CC, interpolation='bilinear', origin='lower', extent=extent)
+    co = ax.contour(CC, levels=budgets, colors='k', origin='lower', extent=extent)
+    ax.clabel(co, budgets[0:-1:2],  # label every second level
                inline=1, fmt='%1.3f')
     trajectory = calc_trajectory(xl - 1, yl - 1)
-    plt.plot(trajectory[:, 0], trajectory[:, 1], 'ro-')
-    plt.colorbar(im)
+    ax.plot(trajectory[:, 0], trajectory[:, 1], 'ro-')
 
-    plt.show()
+plt.tight_layout()
+plt.show()
