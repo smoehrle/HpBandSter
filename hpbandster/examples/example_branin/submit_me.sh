bad7a91a41c11985552ffa4b19435c81e7163a91
smoehrle <26185187+smoehrle@users.noreply.github.com>
2018-07-02 17:55:18 +0200

    Fix cluster

--- hpbandster/examples/example_branin/submit_me.sh
+++ hpbandster/examples/example_branin/submit_me.sh
@@ -1,14 +1,10 @@
 # submit via qsub -t 1-4 submit_me.sh
 
-#$ -q test_core.q
 #$ -cwd
-#$ -o ~moehrles/logs/$JOB_ID/$TASK_ID.o
-#$ -e ~moehrles/logs/$JOB_ID/$TASK_ID.e
-
-# create logs directory if none exists
-mkdir -p "~moehrles/logs/${$JOB_ID}"
+#$ -o $JOB_ID.$TASK_ID.o
+#$ -e $JOB_ID.$TASK_ID.e
 
 # enter the virtual environment
 source ~moehrles/HpBandSter/venv/bin/activate
 
-python3 run_me_cluster.py --run_id "$JOB_ID" --last-task-id "$LAST_TASK_ID" --task_id "$TASK_ID" --master --worker --config $1
+python3 run_me_cluster.py --run_id "$JOB_ID" --last-task-id "$SGE_LAST_TASK_ID" --task_id "$SGE_TASK_ID" --master --worker --config runs/ex1/config.yml
