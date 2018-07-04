252ce8f7543e3fd443066a7cd9486bda3fdb12d9
smoehrle <26185187+smoehrle@users.noreply.github.com>
2018-07-04 10:02:10 +0200

    Fix submit_me.sh

--- hpbandster/examples/example_branin/submit_me.sh
+++ hpbandster/examples/example_branin/submit_me.sh
@@ -1,10 +1,10 @@
 # submit via qsub -t 1-4 submit_me.sh
 
 #$ -cwd
-#$ -o $JOB_ID.$TASK_ID.o
-#$ -e $JOB_ID.$TASK_ID.e
+#$ -o logs/$JOB_ID.$TASK_ID.o
+#$ -e logs/$JOB_ID.$TASK_ID.e
 
 # enter the virtual environment
 source ~moehrles/HpBandSter/venv/bin/activate
 
-python3 run_me_cluster.py --run_id "$JOB_ID" --last-task-id "$SGE_LAST_TASK_ID" --task_id "$SGE_TASK_ID" --master --worker --config runs/ex1/config.yml
+python3 run_me_cluster.py --run-id "$JOB_ID" --last-task-id "$SGE_TASK_LAST" --task-id "$SGE_TASK_ID" --master --worker --config $1
