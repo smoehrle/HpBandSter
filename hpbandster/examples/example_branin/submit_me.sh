dfe6da7e52819092c8578d342b9f236ea93d3c1b
David Elias Kuenstle <kuenstld@informatik.uni-freiburg.de>
2018-07-16 10:48:04 +0200

    Source venv in variable user directory for submit

--- hpbandster/examples/example_branin/submit_me.sh
+++ hpbandster/examples/example_branin/submit_me.sh
@@ -5,6 +5,6 @@
 #$ -e logs/$JOB_ID.$TASK_ID.e
 
 # enter the virtual environment
-source ~moehrles/HpBandSter/venv/bin/activate
+source $HOME/HpBandSter/venv/bin/activate
 
 python3 run_me_cluster.py --run-id "$JOB_ID" --last-task-id "$SGE_TASK_LAST" --task-id "$SGE_TASK_ID" --master --worker --config $1
