a7ea3d0bbbbf16e0cbf065fc459b0e55d77f0869
David-Elias Kuenstle <kuenstld@cs.uni-freiburg.de>
2018-08-01 11:05:54 +0200

    Make ambivalent print more clean

--- hpbandster/examples/example_branin/cache_tasks.py
+++ hpbandster/examples/example_branin/cache_tasks.py
@@ -9,6 +9,6 @@ tasks_df = pd.DataFrame.from_dict(tasks_dict, orient='index')
 tids = tasks_df.tid.values
 print("")
 for tid in tids:
-    print("Download task and dataset {} ...".format(tid))
+    print("Download task {} and the dataset ...".format(tid))
     task = openml.tasks.get_task(tid)
     task.get_dataset()
