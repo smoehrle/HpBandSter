3eaf1b955db6e6966c16f8d6eed7527d56fe2c07
smoehrle <26185187+smoehrle@users.noreply.github.com>
2018-08-04 18:12:23 +0200

    Fix seed generation; add cleanup commands to copy_results script

--- hpbandster/examples/example_branin/copy_results.sh
+++ hpbandster/examples/example_branin/copy_results.sh
@@ -1,5 +1,6 @@
 ssh meta "cd HpBandSter/hpbandster/examples/example_branin/$1 && tar -cvzf result.tar.gz results.$2*"
 scp meta:HpBandSter/hpbandster/examples/example_branin/$1/result.tar.gz $1
+ssh meta "rm HpBandSter/hpbandster/examples/example_branin/$1/result.tar.gz"
 tar -xvzf $1/result.tar.gz -C $1
 rm $1/result.tar.gz
 
